9ffd8b2a2ebdcff1b9424febd176fe23941521be
Patryk Stanek <p.stanek3@gmail.com>
2020-01-27 17:17:13 +0100

    Expanded About page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,12 +10,36 @@ import "./About.scss"
 class About extends React.Component {
     render() {
         return (
-            <div>
-                <span className="mockup">
-                    This is mockup version which is still in developement and is made only for testing.
-                    Content of this page will be added in future updates
-                </span>
-            </div>
+            <section className="about">
+                <div className="about__header">
+                    <h1 className="about__title">
+                        Patrycja Wysowska
+                    </h1>
+                </div>
+                <div className="container about__container">
+                    <h2 className="about__heading">
+                        About me
+                    </h2>
+                    <p className="about__paragraph">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur tenetur adipisci ut molestiae, blanditiis excepturi doloremque possimus ab sed exercitationem nihil labore mollitia facere at maiores eveniet nisi neque odio. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum nihil eveniet commodi maxime suscipit delectus dolorum explicabo, et rem magni autem accusamus hic earum animi sed blanditiis sequi quasi illum?
+                    </p>
+                </div>
+                <div className="about__studio">
+                    <div className="about__box about__box--image">
+                        <img src="./images/studio.jpg" alt="studio" className="about__image"/>
+                    </div>
+                    <div className="about__box about__box--text">
+                        <div className="about__content">
+                            <h3 className="about__heading about__heading--small">
+                                My studio
+                            </h3>
+                            <p className="about__paragraph about__paragraph--full">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt nobis consectetur inventore laudantium maxime veritatis et nam aliquid ipsum, error voluptatem alias debitis odit repudiandae repellendus nesciunt sapiente, voluptas fugit? Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, qui doloribus. Eius quas nihil iure sint dolores vitae labore, quae perspiciatis perferendis quod nobis. Eum rerum provident minus facere! Ad?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
         )
     }
 }
